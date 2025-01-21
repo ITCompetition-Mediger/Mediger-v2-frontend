e8bdc0ed3431206edc6874e9104a3586ff7057c5
MyungJiwoo <1206jiwoo@gmail.com>
2025-01-21 15:58:38 +0900

    #5 refactor: 공통 레이아웃 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage.tsx';
+import SignupPage from './pages/SignupPage.tsx';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" Component={LoginPage} />
+        <Route path="/login" Component={LoginPage} />
+        <Route path="/signup" Component={SignupPage} />
       </Routes>
     </Router>
   );
